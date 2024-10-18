1017198cea8145237932ffa7d0cb98e867ca4eed
Aynun Nissa Setiawan <aynun@GTJKTMAC-159.local>
2024-10-19 01:30:10 +0700

    feat: question 4 - get users

---
+++ src/components/users/UserTable.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+interface IData {
+  id: number,
+  name: string,
+  email: string,
+  gender: string,
+  status: string,
+}
+
+interface IProps {
+  data: IData[]
+}
+
+export default function UserTable({ data }: IProps) {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell sx={{ fontWeight: 700 }}>ID</TableCell>
+            <TableCell sx={{ fontWeight: 700 }} align="right">Name</TableCell>
+            <TableCell sx={{ fontWeight: 700 }} align="right">Email</TableCell>
+            <TableCell sx={{ fontWeight: 700 }} align="right">Gender</TableCell>
+            <TableCell sx={{ fontWeight: 700 }} align="right">Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data?.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell align="right">{row.name}</TableCell>
+              <TableCell align="right">{row.email}</TableCell>
+              <TableCell align="right">{row.gender}</TableCell>
+              <TableCell align="right">{row.status}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
---
+++ src/pages/q4-get-users/index.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { Box, Container, Divider, Typography } from '@mui/material';
+import UserTable from '@/components/users/UserTable';
+
+interface IData {
+  id: number,
+  name: string,
+  email: string,
+  gender: string,
+  status: string,
+}
+
+interface IProps {
+  data: IData[],
+  error: boolean
+}
+
+export default function GetUsers({ data, error }: IProps) {
+  return (
+    <Container>
+      <Box py={4}>
+        <Typography fontWeight={700} variant='h5' component='h1'>List Users</Typography>
+        <Divider sx={{ mb: 4, mt: 2}} />
+        <UserTable data={data} />
+      </Box>
+    </Container>
+  );
+}
+
+export async function getServerSideProps() {
+  try {
+    const response = await fetch('https://gorest.co.in/public/v2/users');
+
+    const errorCode = response.ok ? null : response.status;
+
+    const result = await response.json();
+
+    return {
+      props: {
+        data: result || [],
+        error: errorCode
+      }
+    }
+  } catch (error) {
+    console.error(`Error: ${error}`)
+    return {
+      props: {
+        data: [],
+        error: 500
+      }
+    }
+  }
+}
