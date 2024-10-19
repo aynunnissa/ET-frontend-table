7338dc3d7d47208ee821ae48ec5b07c55acc05f6
Aynun Nissa Setiawan <aynun@GTJKTMAC-159.local>
2024-10-19 19:52:30 +0700

    perf: add meta tag

--- src/pages/q3-table-management/index.tsx
+++ src/pages/q3-table-management/index.tsx
@@ -1,13 +1,23 @@
 import InvoiceTable from "@/components/table-management/InvoiceTable";
 import { Box, Divider, Typography } from "@mui/material";
+import Head from "next/head";
 
 const TableManagement = () => {
   return(
-    <Box>
-      <Typography my={4} px={8} variant="h5" component="h1">Manage Invoices</Typography>
-      <Divider />
-      <InvoiceTable />
-    </Box>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <title>Invoice Management</title>
+        <meta name="description" content="Create, send, and track invoices effortlessly, all in one place." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Box>
+        <Typography my={4} px={8} variant="h5" component="h1">Manage Invoices</Typography>
+        <Divider />
+        <InvoiceTable />
+      </Box>
+    </>
   );
 }
 
--- src/pages/q4-get-users/index.tsx
+++ src/pages/q4-get-users/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Box, Container, Divider, Typography } from '@mui/material';
 import UserTable from '@/components/users/UserTable';
 import { BASE_URL } from '@/lib/client';
+import Head from 'next/head';
 
 interface IData {
   id: number,
@@ -18,13 +19,22 @@ interface IProps {
 
 export default function GetUsers({ data }: IProps) {
   return (
-    <Container>
-      <Box py={4}>
-        <Typography fontWeight={700} variant='h5' component='h1'>List Users</Typography>
-        <Divider sx={{ mb: 4, mt: 2}} />
-        <UserTable data={data} />
-      </Box>
-    </Container>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <title>List Users</title>
+        <meta name="description" content="Manage and view all user profiles in one place with our easy-to-use user list page." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container>
+        <Box py={4}>
+          <Typography fontWeight={700} variant='h5' component='h1'>List Users</Typography>
+          <Divider sx={{ mb: 4, mt: 2}} />
+          <UserTable data={data} />
+        </Box>
+      </Container>
+    </>
   );
 }
 
