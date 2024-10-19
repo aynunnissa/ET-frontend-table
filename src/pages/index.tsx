b620cc8bbb513ab154a91cba08c3c10411d3090c
Aynun Nissa Setiawan <aynun@GTJKTMAC-159.local>
2024-10-19 08:06:26 +0700

    feat: add list features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
-import Image from "next/image";
+import LaunchIcon from '@mui/icons-material/Launch';
 import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
+import { Typography } from "@mui/material";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,91 +29,19 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
-          <Image
-            className={styles.logo}
-            src="https://nextjs.org/icons/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
+          <Typography variant="h6" component="h1" fontWeight={700}>Available Features</Typography>
           <ol>
             <li>
-              Get started by editing <code>src/pages/index.tsx</code>.
+              <Link style={{ color: 'blue', textDecoration: 'underline' }} href="/q3-table-management">Question 3: Buat Table dengan Fungsionalitas: Search, Edit, Add, and Delete <LaunchIcon /></Link>
             </li>
-            <li>Save and see your changes instantly.</li>
+            <li>
+              <Link style={{ color: 'blue', textDecoration: 'underline' }} href="/q4-get-users">Question 4: Akses API User dan Munculkan dalam Bentuk Table <LaunchIcon /></Link>
+            </li>
+            <li>
+              <Link style={{ color: 'blue', textDecoration: 'underline' }} href="/q5-register-users">Question 5: Buat Tampilan untuk Register User <LaunchIcon /></Link>
+            </li>
           </ol>
-
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.logo}
-                src="https://nextjs.org/icons/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
-            </a>
-          </div>
         </main>
-        <footer className={styles.footer}>
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
       </div>
     </>
   );
